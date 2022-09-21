2b043f76466151f67993aba5a4a50e993e9558bb
Sergio Pedri <sergio0694@live.com>
2022-09-21 18:20:12 +0200

    Move and rename shader to separate folder

---
+++ ExampleGallery/PixelShaders/AnimatedColorsShader.cs
@@ -0,0 +1,34 @@
+﻿using ComputeSharp;
+using ComputeSharp.D2D1;
+
+namespace ExampleGallery.PixelShaders;
+
+/// <summary>
+/// A shader showing an animated colored background.
+/// </summary>
+[D2DInputCount(0)]
+[D2DShaderProfile(D2D1ShaderProfile.PixelShader40)]
+[D2DRequiresScenePosition]
+[AutoConstructor]
+public readonly partial struct AnimatedColorsShader : ID2D1PixelShader
+{
+    public readonly float time;
+    public readonly int width;
+    public readonly int height;
+
+    /// <inheritdoc/>
+    public float4 Execute()
+    {
+        // Normalized screen space UV coordinates from 0.0 to 1.0
+        float2 uv = D2D.GetScenePosition().XY / new float2(width, height);
+
+        // Time varying pixel color
+        float3 col = 0.5f + 0.5f * Hlsl.Cos(time + new float3(uv, uv.X) + new float3(0, 2, 4));
+
+        // Trim the red out
+        col = new float3(0, col.YZ);
+
+        // Output to screen
+        return new(col, 1f);
+    }
+}
--- ExampleGallery/ShaderPlayground.xaml.cs
+++ ExampleGallery/ShaderPlayground.xaml.cs
@@ -1,6 +1,5 @@
-﻿using ComputeSharp;
-using ComputeSharp.D2D1;
-using ComputeSharp.D2D1.Interop;
+﻿using ComputeSharp.D2D1.Interop;
+using ExampleGallery.PixelShaders;
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Effects;
 using Microsoft.Graphics.Canvas.UI.Xaml;
@@ -8,7 +7,6 @@ using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
-using System.Diagnostics;
 using Windows.Graphics.Effects;
 using WinRT;
 
@@ -72,47 +70,20 @@ public sealed partial class ShaderPlayground : UserControl
         else
         {
             // Get the shader bytecode
-            byte[] bytecode = D2D1PixelShader.LoadBytecode<MyShader>().ToArray();
+            byte[] bytecode = D2D1PixelShader.LoadBytecode<AnimatedColorsShader>().ToArray();
 
             // Create a Win2D pixel shader effect with the shader bytecode
             PixelShaderEffect effect = new(bytecode);
 
             // Set the shader properties in the constant buffer
-            effect.Properties[nameof(MyShader.time)] = time;
-            effect.Properties[nameof(MyShader.width)] = (int)sender.ActualWidth;
-            effect.Properties[nameof(MyShader.height)] = (int)sender.ActualHeight;
+            effect.Properties[nameof(AnimatedColorsShader.time)] = time;
+            effect.Properties[nameof(AnimatedColorsShader.width)] = (int)sender.ActualWidth;
+            effect.Properties[nameof(AnimatedColorsShader.height)] = (int)sender.ActualHeight;
 
             // Draw the pixel shader
             args.DrawingSession.DrawImage(effect);
 
             args.DrawingSession.DrawText("The background is a custom D2D1 pixel shader!", 100, 0, Colors.White);
         }
-    }
-}
-
-[D2DInputCount(0)]
-[D2DShaderProfile(D2D1ShaderProfile.PixelShader40)]
-[D2DRequiresScenePosition]
-[AutoConstructor]
-public readonly partial struct MyShader : ID2D1PixelShader
-{
-    public readonly float time;
-    public readonly int width;
-    public readonly int height;
-
-    /// <inheritdoc/>
-    public float4 Execute()
-    {
-        // Normalized screen space UV coordinates from 0.0 to 1.0
-        float2 uv = D2D.GetScenePosition().XY / new float2(width, height);
-
-        // Time varying pixel color
-        float3 col = 0.5f + 0.5f * Hlsl.Cos(time + new float3(uv, uv.X) + new float3(0, 2, 4));
-
-        // Trim the red out
-        col = new float3(0, col.YZ);
-
-        // Output to screen
-        return new(col, 1f);
     }
 }
